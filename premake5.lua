0db45370f9f271d52914f6c5733ca10d628d60c0
Gary <qiuhuidi31@Gmail.com>
2024-08-05 23:47:19 +0800

    Add premake5.lua and modify .gitignore for Pika

---
+++ premake5.lua
@@ -0,0 +1,41 @@
+project "Box2D"
+    kind "StaticLib"
+    language "C++"
+    staticruntime "on"
+
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "src/**.h",
+        "src/**.cpp",
+        "include/**.h"
+    }
+
+    includedirs
+    {
+        "include"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+        cppdialect "C++20"
+
+    filter "system:linux"
+        pic "On"
+        systemversion "latest"
+        cppdialect "C++20"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
+
+    filter "configurations:Dist"
+        runtime "Release"
+        optimize "on"
+        symbols "off"
